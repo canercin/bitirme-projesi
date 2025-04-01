f16b5d2f48a60e2a0a677c10b949f10a6331b6d0
Nursena <106694139+nurssy@users.noreply.github.com>
2025-04-01 13:03:48 +0300

    v2commit

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // İlk açılışta HomeComponent yüklenecek
+  { path: 'user', component: UserPageComponent }  // /user yoluna gidince UserPageComponent gelecek
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
